13176f39699159a82ce931ed2fec5e27ff1c84bd
Adesina Quayum <quayumadesina@gmail.com>
2026-03-11 16:51:44 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import Project from './Project'
-import Login from './Signup'
-import Loginn from './Loginn'
-import Friday from './Friday'
 import Testing from './Testing'
 
 
